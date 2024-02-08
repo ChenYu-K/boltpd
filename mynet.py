6072b77310fe5619a58d240da5b7652bb527eb25
Yu Chen <cy519234505@gmail.com>
2024-02-08 15:38:08 +0900

    240208

--- mynet.py
+++ mynet.py
@@ -1,5 +1,4 @@
 import torch
-#import torchvision.transforms as T
 import torchvision.transforms.transforms as T
 from torch.utils.data import Dataset
 from torchvision import transforms
@@ -41,11 +40,10 @@ class Bottleneck(nn.Module):
         self.conv3 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel*self.expansion, kernel_size=1, stride=1, bias=False)   # H,W不变。C: out_channel -> 4*out_channel
         self.bn3 = nn.BatchNorm2d(num_features=out_channel*self.expansion)
         self.relu = nn.ReLU(inplace=True)
-
         # Add SEBlock
-        #self.seblock = SEBlock(out_channel*self.expansion)
+        self.seblock = SEBlock(out_channel*self.expansion)
         self.downsample = downsample
-
+    
     def forward(self, x):
         identity = x    # 将原始输入暂存为shortcut的输出
         if self.downsample is not None:
@@ -62,13 +60,146 @@ class Bottleneck(nn.Module):
         out = self.conv3(out)
         out = self.bn3(out)
 
-        # Apply SEBlock
-        #out = self.seblock(out)
+        #Apply SEBlock
+        out = self.seblock(out)
 
         out += identity     # 残差连接0 n
         out = self.relu(out)
 
         return out
+
+
+class ModResnet(nn.Module):
+    """
+    __init__
+        block: 堆叠的基本模块
+        block_num: 基本模块堆叠个数,是一个list,对于resnet50=[3,4,6,3]
+        num_classes: 全连接之后的分类特征维度
+
+    _make_layer
+        block: 堆叠的基本模块
+        channel: 每个stage中堆叠模块的第一个卷积的卷积核个数，对resnet50分别是:64,128,256,512
+        block_num: 当期stage堆叠block个数
+        stride: 默认卷积步长
+    """
+    def __init__(self, block, block_num, num_classes, num_attention_heads=4):
+        super(ResNet, self).__init__()
+        self.in_channel = 64    # conv1的输出维度
+
+        self.conv1 = nn.Conv2d(in_channels=2, out_channels=self.in_channel, kernel_size=7, stride=2, padding=3, bias=False)     # H/2,W/2。C:3->64
+        self.bn1 = nn.BatchNorm2d(self.in_channel)
+        self.relu = nn.ReLU(inplace=True)
+        self.activation = nn.LeakyReLU(negative_slope=0.01)
+        self.sigmoid = nn.Sigmoid()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)     # H/2,W/2。C不变
+        self.layer1 = self._make_layer(block=block, channel=64, block_num=block_num[0], stride=1)   # H,W不变。downsample控制的shortcut，out_channel=64x4=256
+        self.layer2 = self._make_layer(block=block, channel=128, block_num=block_num[1], stride=2)  # H/2, W/2。downsample控制的shortcut，out_channel=128x4=512
+        self.layer3 = self._make_layer(block=block, channel=256, block_num=block_num[2], stride=2)  # H/2, W/2。downsample控制的shortcut，out_channel=256x4=1024
+        self.layer4 = self._make_layer(block=block, channel=512, block_num=block_num[3], stride=2)  # H/2, W/2。downsample控制的shortcut，out_channel=512x4=2048
+
+        # 添加自注意力层
+        self.self_attention = nn.MultiheadAttention(embed_dim=2048, num_heads=num_attention_heads)
+
+        # Add SEBlocks after each residual block in the feature extraction layers
+        self.seblock1 = SEBlock(64 * block.expansion)
+        self.seblock2 = SEBlock(128 * block.expansion)
+        self.seblock3 = SEBlock(256 * block.expansion)
+        self.seblock4 = SEBlock(512 * block.expansion)
+
+        self.fusion4 = MultiScaleFusion(1024 * block.expansion)
+        self.fusion3 = MultiScaleFusion(512 * block.expansion)
+        self.fusion2 = MultiScaleFusion(256 * block.expansion)
+        self.fusion1 = MultiScaleFusion(128 * block.expansion)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))  # 将每张特征图大小->(1,1)，则经过池化后的输出维度=通道数
+        self.fc1 = nn.Linear(in_features=512*block.expansion, out_features=1024) #in=2048,out=1024
+        self.dropout = nn.Dropout(0.5) #dropout rate
+        self.fc2 = nn.Linear(in_features=2048, out_features=1024) #in=1024, out=1
+        self.fc3 = nn.Linear(in_features=1024, out_features=num_classes) #in=512, out=1
+        #self.fc4 = nn.Linear(in_features=64, out_features=num_classes) #in=128, out=1
+
+        # Upsampling layers for feature fusion
+        self.upsample = lambda x: nn.functional.interpolate(x, scale_factor=2, mode='bilinear', align_corners=None).to(x.device)
+        #self.upsample = nn.functional.interpolate(scale_factor=2, mode='bilinear', align_corners=True)
+        
+        for m in self.modules():    # 权重初始化
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def _make_layer(self, block, channel, block_num, stride=1):
+        downsample = None   # 用于控制shorcut路的
+        if stride != 1 or self.in_channel != channel*block.expansion:   # 对resnet50：conv2中特征图尺寸H,W不需要下采样/2，但是通道数x4，因此shortcut通道数也需要x4。对其余conv3,4,5，既要特征图尺寸H,W/2，又要shortcut维度x4
+            downsample = nn.Sequential(
+                nn.Conv2d(in_channels=self.in_channel, out_channels=channel*block.expansion, kernel_size=1, stride=stride, bias=False), # out_channels决定输出通道数x4，stride决定特征图尺寸H,W/2
+                nn.BatchNorm2d(num_features=channel*block.expansion))
+
+        layers = []  # 每一个convi_x的结构保存在一个layers列表中，i={2,3,4,5}
+        layers.append(block(in_channel=self.in_channel, out_channel=channel, downsample=downsample, stride=stride)) # 定义convi_x中的第一个残差块，只有第一个需要设置downsample和stride
+        self.in_channel = channel*block.expansion   # 在下一次调用_make_layer函数的时候，self.in_channel已经x4
+
+        for _ in range(1, block_num):  # 通过循环堆叠其余残差块(堆叠了剩余的block_num-1个)
+            layers.append(block(in_channel=self.in_channel, out_channel=channel))
+
+        return nn.Sequential(*layers)   # '*'的作用是将list转换为非关键字参数传入
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.activation(x)
+        x = self.maxpool(x)
+
+        x1 = self.layer1(x)
+        #x1 = self.fusion1(x1)
+        #x1 = self.seblock1(x1)
+        x2 = self.layer2(x1)
+        #x2 = self.seblock2(x2)
+        #x2 = self.fusion2(x2)
+        x3 = self.layer3(x2)
+        #x3 = self.seblock3(x3)
+        #x3 = self.fusion3(x3)
+        x4 = self.layer4(x3)
+        #x4 = self.seblock4(x4)
+        #x4 = self.fusion4(x4)
+
+
+        x = self.avgpool(x4)
+        x = torch.flatten(x, 1)
+
+        # 添加自注意力层
+        #x = x.unsqueeze(0)  # 添加一个维度以适应自注意力层的输入要求
+        #x, _ = self.self_attention(x, x, x)
+        #x = x.squeeze(0)  # 移除添加的维度
+        
+        x = self.relu(self.fc1(x))
+        #x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+
+        return x
+
+class MultiScaleFusion(nn.Module):
+    def __init__(self,out_channels):
+        super(MultiScaleFusion, self).__init__()
+        self.conv1 = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=5, stride=1, padding=2)
+        # 定义融合的卷积层
+        self.fusion_conv = nn.Conv2d(4*out_channels, out_channels, kernel_size=1)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.conv2(x)
+        x3 = self.conv3(x)
+        out = torch.cat([x,x1, x2, x3], dim=1)
+        # 对融合的特征图进行卷积操作
+        out = self.fusion_conv(out)
+        out = self.bn(out)
+        out = self.relu(out)
+        return out
+    
+
 
 
 class ResNet(nn.Module):
---
+++ train.py
@@ -0,0 +1,125 @@
+def testAccuracy():
+    net.eval()
+    accuracy = 0.0
+    total = 0.0
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #device = torch.device("cpu")
+    x=[]
+    y=[]
+    np.array(x)
+    np.array(y)
+    # net=torch.load('')
+    with torch.no_grad():
+        for i, (images, labels) in enumerate(valid_loader, 0):
+            #images, labels = dataiter.next()
+            # 输入数据进行预测
+            # get the inputs
+            images = Variable(images.to(device))
+            labels = Variable(labels.to(device))
+            predata = net(images)
+            #predata, labedata = predict(net, valid_loader)
+            loss=loss_func(predata, labels)
+            loss += loss.item()
+            x=np.append(x,[i[0] for i in labels.data.cpu().numpy()])
+            y=np.append(y,np.array([i[0] for i in predata.data.cpu().numpy()]))
+    preloss = np.mean(loss.data.cpu().numpy()) #preloss = loss/(i+1)
+    r2 = 1 - np.mean((y - x) ** 2) / np.mean((x - x.mean()) ** 2)
+    vmse = np.mean((y - x) ** 2)
+    return(r2,preloss,vmse)
+
+def train(num_epochs,device):
+    import time
+    start_time = time.process_time()
+    best_accuracy = 0.0
+
+    # Define your execution device
+    #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #device = torch.device("cpu")
+    k=0
+    predata=[]
+    labeldata=[]
+    mseloss=[]
+    trainnum=[]
+    epochout=[]
+    epochV=[]
+    vloss=[]
+    vr2=[]
+    # Convert model parameters and buffers to CPU or Cuda
+    net.to(device)
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        running_acc = 0.0
+        for i, (images, labels) in enumerate(train_loader, 0):
+            #images, labels = dataiter.next()
+            # get the inputs
+            images = Variable(images.to(device))
+            labels = Variable(labels.to(device))
+            prediction = net(images)
+            #print(prediction,labels)
+            #print('Prediction data is:',prediction.data.cpu().numpy()[0])
+            #print('label data is: ',labels.data.cpu().numpy())
+            # 计算预测值与真值误差，注意参数顺序问题
+            # 第一个参数为预测值，第二个为真值
+            loss = loss_func(prediction, labels)*100
+            # 开始优化步骤
+            # 每次开始优化前将梯度置为0
+            optimizer.zero_grad()
+            # 误差反向传播
+            loss.backward()
+            # 按照最小loss优化参数
+            optimizer.step()
+            running_loss += loss.item()
+
+            if (i+1) % 10 == 0:
+                # print every 1000 (twice per epoch)
+                #print(f"epoch #{epoch+1} Iteration #{i+1} loss: {loss_value}")
+                print('[Epoch: %d/%d]- %d/%d | loss: %.3f | predict-True: %5d - %5d' %
+                      (epoch + 1,num_epochs, i + 1,len(train_loader), loss.item(), prediction.data.cpu().numpy()[1]*116,labels.data.cpu().numpy()[1]*116))
+                mseloss.append(running_loss/10)
+                trainnum.append(i+1)
+                epochout.append(epoch+1)
+                result=pd.DataFrame({'epoch':epochout,'trainnum':trainnum,'loss':mseloss})
+                result.to_csv("./result/"+casename+"/"+casename+"-result.csv",index=False,sep=',')
+                # Tensorboard
+                writer.add_scalar('Training loss',running_loss / 10,epoch * len(train_loader) + i)
+                # writer.add_figure('predictions vs. actuals',
+                #                   plot_classes_preds(net, inputs, labels),
+                #                   global_step=epoch * len(trainloader) + i)
+                # zero the loss
+                running_loss = 0.0
+
+            predata.append(prediction.data.cpu().numpy()[0])
+            labeldata.append(labels.data.cpu().numpy()[0])
+
+        r2,validloss,vmse = testAccuracy()
+        vloss.append(validloss)
+        vr2.append(r2)
+        epochV.append(epoch+1)
+        writer.add_scalar('valid_loss',validloss,epoch+1)
+        vresult=pd.DataFrame({'epoch':epochV,'vloss':vloss,'vMSE':vmse,'vr2':vr2})
+        vresult.to_csv("./result/"+casename+"/"+casename+"-result-valid.csv",index=False,sep=',')
+        print('[Epoch: %d/%d] ======>| Valid Loss: %.3f ,MSE: %.3f | R^2 is %.3f' % (epoch+1,num_epochs,validloss,vmse,r2))
+        if validloss <= 0.005:
+            k +=1
+            print('Validloss K=',k)
+            if k == 3 and r2>0.90:
+                torch.save(net, casename+'-OK'+'.pt')
+
+            if k == 3 and r2>0.95:
+                print('Traing is end, Due to Validloss consecutively less than 0.005.')
+                break
+        else: k = 0
+
+
+    print('Finished Training')
+    ttime=(np.array(epochout)-1)*len(train_loader)+np.array(trainnum)
+    plt.figure()
+    plt.legend()
+    plt.grid(True)
+    plt.ylim(0,0.1)
+    plt.ylabel("Mean Squared Error")
+    plt.xlabel("train times")
+    plt.plot(ttime,mseloss)
+    plt.savefig('tranloss.png', dpi=1200) 
+    end_time = time.process_time()
+    print("Use time:", (end_time-start_time)/3600)
